520b7a061b3b672374b4580d5d55742122e915b7
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-02 21:55:06 -0600

    Complete line 8 (grepl)

--- R/week3.R
+++ R/week3.R
@@ -5,3 +5,4 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df <- read.csv(file = "../data/week3.csv", header = TRUE)
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[!grepl("2017-06", raw_df$timeEnd), ]
