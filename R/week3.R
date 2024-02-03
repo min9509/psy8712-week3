c9d55b901a4a5161a9bce83060d57f0836c0302a
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-02 22:11:44 -0600

    Complete Line 12

--- R/week3.R
+++ R/week3.R
@@ -6,4 +6,8 @@ raw_df <- read.csv(file = "../data/week3.csv", header = TRUE)
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- raw_df[!grepl("2017-06", raw_df$timeEnd), ]
-clean_df <- clean_df[clean_df$q6 == 1,]
+clean_df <- clean_df[clean_df$q6 == 1,] 
+# Analysis
+?difftime
+timeSpent <- difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart, units = "auto")
+timeSpent
