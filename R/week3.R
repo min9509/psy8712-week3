0046bc830f3b0dd696cffc879e43ab84744d16f1
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-02 22:14:42 -0600

    Modify line 10~12 and complete line 13

--- R/week3.R
+++ R/week3.R
@@ -7,7 +7,8 @@ raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- raw_df[!grepl("2017-06", raw_df$timeEnd), ]
 clean_df <- clean_df[clean_df$q6 == 1,] 
+
 # Analysis
 ?difftime
-timeSpent <- difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart, units = "auto")
-timeSpent
+timeSpent <- difftime(time1 = clean_df$timeEnd, time2 = clean_df$timeStart, units = "secs")
+hist(as.numeric(timeSpent))
